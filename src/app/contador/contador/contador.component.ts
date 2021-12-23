0476402a4cdd83a7eac7e76c5ea1d518242bfd1b
ALEJANDRO MICHELL LOZA ZENIL <aloza@ad.elektra.com.mx>
2021-12-23 17:12:12 -0600

    segundo commit

---
+++ src/app/contador/contador/contador.component.ts
@@ -0,0 +1,31 @@
+import { Component } from "@angular/core";  
+
+@Component({
+    selector:'app-contador',
+    template:`
+        <h1> {{ title }} </h1>
+
+        <h3>La base es: <strong>{{ base }}</strong></h3>
+
+        <button (click)=" acumular(+base); "> +1 </button>
+
+        <span> {{ numero }} </span>
+
+        <button (click)=" acumular(-base); "> -1 </button>
+    `
+})
+export class ContadorComponent{
+    title:string = 'contador app';
+    numero: number = 10;
+    base: number = 5;
+    /*sumar(){
+      this.numero += 1;
+    }
+    restar(){
+      this.numero -= 1;
+    }*/
+    acumular( valor: number )
+    {
+      this.numero += valor;
+    }
+} 
